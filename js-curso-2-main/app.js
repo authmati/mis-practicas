d41c17cf2ea68f217e0898377c80ca02b8957600
authmati <matiasayala517@gmail.com>
2025-01-16 16:53:39 -0300

    Realizando las actividades propuestas

--- js-curso-2-main/app.js
+++ js-curso-2-main/app.js
@@ -1,1 +1,21 @@
+let titulo = document.querySelector("#principal");
 
+titulo.innerHTML = "Hora del Desafío";
+
+function alertaConsola() {
+    console.log("El botón fue clicado");
+}
+
+function preguntaCiudad() {
+    let ciudad = prompt("Dime una ciudad de Brasil:");
+
+    if (ciudad) {
+        alert(`Estuve en ${ciudad} y me acorde de ti.`);
+    } else {
+        alert("No has ingresado una ciudad.");
+    }
+}
+
+function botoncitoAlerta() {
+    alert("Yo amo JS!");
+}
